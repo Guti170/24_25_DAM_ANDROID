95b556fdfc14fee6ba0b0d25525ff7305036438d
Miguel Angel Gutierrez <122835438+Guti170@users.noreply.github.com>
2024-09-26 19:21:01 +0200

    Modificaciones 1.0 Proyecto Controles Con Funciones

--- ControlesConFuncionalidad/app/src/main/java/com/example/controlesconfuncionalidad/MainActivity.kt
+++ ControlesConFuncionalidad/app/src/main/java/com/example/controlesconfuncionalidad/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.controlesconfuncionalidad
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +20,19 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val miTag = "Miguel Angel"
+        val ptNombre = findViewById<EditText>(R.id.ptNombre)
+        val btAceptar = findViewById<Button>(R.id.btAceptar)
+        val btBorrar = findViewById<Button>(R.id.btBorrar)
+
+        btAceptar.setOnClickListener {
+            Log.i(miTag,"Hola ${ptNombre.text}")
+            Toast.makeText(this, "Hola ${ptNombre.text}", Toast.LENGTH_LONG).show()
+        }
+        btBorrar.setOnClickListener {
+            ptNombre.text.clear()
+        }
+
     }
 }
