4f2b2eb83568e1aae04afce85fdc776f62a3153f
Miguel Angel Gutierrez <122835438+Guti170@users.noreply.github.com>
2024-10-03 19:15:05 +0200

    Proyecto mas controles Image View, Binding...

--- BindingRadioButtonsImagenesSwitchSeek/app/src/main/java/com/example/bindingradiobuttonsimagenesswitchseek/MainActivity.kt
+++ BindingRadioButtonsImagenesSwitchSeek/app/src/main/java/com/example/bindingradiobuttonsimagenesswitchseek/MainActivity.kt
@@ -2,14 +2,14 @@ package com.example.bindingradiobuttonsimagenesswitchseek
 
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.bindingradiobuttonsimagenesswitchseek.databinding.ActivityMainBinding
+import modelo.PedidoPizzeria
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -28,13 +28,46 @@ class MainActivity : AppCompatActivity() {
         }
 
         val miTag = "Miguel Angel"
+        var currentImage = R.drawable.ic_comida_foreground
+        var imagen1 = R.drawable.ic_comida_foreground
+        var imagen2 = R.drawable.ic_pizza
 
         binding.btAceptar.setOnClickListener {
-            Log.i(miTag,"Hola ${binding.ptNombre.text}")
-            Toast.makeText(this, "Hola ${binding.ptNombre.text}", Toast.LENGTH_LONG).show()
+            if(binding.stLicencia.isChecked){
+
+            } else {
+                Toast.makeText(this, "Debes aceptar la licencia", Toast.LENGTH_SHORT).show()
+            }
         }
+
+        val order = PedidoPizzeria(binding.ptNombre.text.toString(),
+            binding.cbBacon.isChecked,
+            binding.cbCebolla.isChecked,
+            binding.cbQueso.isChecked,
+            binding.rbFino.isChecked,
+            binding.rbGordo.isChecked)
+        Log.d(miTag, order.toString())
+
         binding.btBorrar.setOnClickListener {
             binding.ptNombre.text.clear()
+            binding.cbBacon.isChecked = false
+            binding.cbCebolla.isChecked = false
+            binding.cbQueso.isChecked = false
+            binding.stLicencia.isChecked = false
+            binding.rbFino.isChecked = false
+            binding.rbGordo.isChecked = false
+        }
+        binding.ibCambiarImagen.setOnClickListener {
+            if (currentImage == imagen1) {
+                binding.ivImagen.setImageResource(imagen2)
+                currentImage = imagen2
+            } else {
+                binding.ivImagen.setImageResource(imagen1)
+                currentImage = imagen1
+            }
+        }
+        binding.ivImagen.setOnClickListener {
+            binding.ivImagen.setImageResource(R.drawable.pizza)
         }
 
     }
---
+++ BindingRadioButtonsImagenesSwitchSeek/app/src/main/java/modelo/PedidoPizzeria.kt
@@ -0,0 +1,10 @@
+package modelo
+
+data class PedidoPizzeria(
+    val nombre: String,
+    val bacon: Boolean,
+    val cebolla: Boolean,
+    val queso: Boolean,
+    val bordeFino: Boolean,
+    val bordeGordo: Boolean,
+)
