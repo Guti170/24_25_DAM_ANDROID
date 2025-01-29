ebe8558568d39fdfd2e899c908ca0fd5898490a8
Miguel Angel <elguti17.mg@gmail.com>
2025-01-29 08:18:58 +0100

    PROYECTO CLIENTE DESAYUNO - INICIO DISEÑO PRINCIPAL - REGISTRARSE - REGISTRASE GOOGLE ICONO APLICACION INSERCION DE RECYCLE VIEW EN BEBIDAS Y COMIDAS PASAR DATOS DE LA RECYCLE VIEW A LA PRINCIPAL Y HAGA LAS SUMAS DE LOS VALORES ENERGETICOS DISEÑO FONDOS Y DISEÑO LETRAS ALERTAS EN RECYCLE VIEW, ALERTA EN INICIAR SESION, MEJORAR DISEÑO LOGIN SQLITE - BASE DE DATOS HECHA FIREBASE - UTILIZACION DE STORAGE CON LAS IMAGENES COMIDA Y BEBIDA FIREBASE - BASE DE DATOS HECHA ESTILO- BARRA MENU HOME BARRA MENU - INSERTADA CORRECTAMENTE EN EL HOME FARGMENTS - HECHO LA INTERFAZ, SOLO FALTA QUE FUNCIONE CORRECTAMENTE
    - APLICACION EN FUNCIONAMIENTO

--- ProyectoClienteDesayuno/app/src/main/java/registrarIniciarUsuario/EditUserFragment.kt
+++ ProyectoClienteDesayuno/app/src/main/java/registrarIniciarUsuario/EditUserFragment.kt
@@ -1,98 +1,192 @@
 package registrarIniciarUsuario
 
+import android.Manifest
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
-import android.widget.ImageView
 import android.widget.Toast
-import com.example.proyectoclientedesayuno.R
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import com.example.proyectoclientedesayuno.databinding.FragmentEditUserBinding
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import java.io.ByteArrayOutputStream
+import java.util.UUID
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [EditUserFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class EditUserFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: FragmentEditUserBinding? = null
+    private val binding get() = _binding!!
 
-    private lateinit var imageView: ImageView
-    private lateinit var editTextAltura: EditText
-    private lateinit var editTextPeso: EditText
-
-    lateinit var binding: FragmentEditUserBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = FragmentEditUserBinding.inflate(layoutInflater)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private val REQUEST_CODE_PERMISSIONS = 10
+    private val REQUEST_CODE_SELECT_IMAGE = 1
+    private val REQUEST_CODE_TAKE_PICTURE = 2
+    private var imageUri: Uri? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_edit_user, container, false)
+    ): View {
+        _binding = FragmentEditUserBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        imageView = view.findViewById(R.id.user_imagen)
-        editTextAltura = view.findViewById(R.id.user_altura)
-        editTextPeso = view.findViewById(R.id.user_peso)
+        requestPermissions()
 
         binding.saveButton.setOnClickListener {
-            val menu = hashMapOf(
-                "imagen" to imageView,
-                "altura" to editTextAltura,
-                "peso" to editTextPeso
-            )
-
-            val db = FirebaseFirestore.getInstance()
-            db.collection("informacion")
-                .add(menu)
-                .addOnSuccessListener { documentReference ->
-                    Toast.makeText(requireContext(), "Usuario guardado correctamente con ID: ${documentReference.id}", Toast.LENGTH_SHORT).show()
-                }
-                .addOnFailureListener { e ->
-                    Toast.makeText(requireContext(), "Error al guardar el usuario: ${e.message}", Toast.LENGTH_SHORT).show()
-                }
+            if (imageUri != null) {
+                uploadImageToFirebaseStorage(imageUri!!)
+            } else {
+                saveDataToFirestore(null)
+            }
         }
 
         binding.selectImageButton.setOnClickListener {
             val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
             startActivityForResult(intent, REQUEST_CODE_SELECT_IMAGE)
         }
+
+        binding.takePictureButton.setOnClickListener {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(intent, REQUEST_CODE_TAKE_PICTURE)
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if (requestCode == REQUEST_CODE_SELECT_IMAGE && resultCode == Activity.RESULT_OK && data != null) {
-            val selectedImageUri = data.data
-            imageView.setImageURI(selectedImageUri)
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                REQUEST_CODE_SELECT_IMAGE -> {
+                    imageUri = data?.data
+                    binding.userImagen.setImageURI(imageUri)
+                }
+
+                REQUEST_CODE_TAKE_PICTURE -> {
+                    val imageBitmap = data?.extras?.get("data") as Bitmap
+                    binding.userImagen.setImageBitmap(imageBitmap)
+                    imageUri = getImageUri(imageBitmap)
+                }
+            }
         }
     }
 
-    companion object {
-        private const val REQUEST_CODE_SELECT_IMAGE = 1
+    private fun getImageUri(inImage: Bitmap): Uri? {
+        val bytes = ByteArrayOutputStream()
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes)
+        val path = MediaStore.Images.Media.insertImage(
+            requireContext().contentResolver,
+            inImage,
+            "Title",
+            null
+        )
+        return Uri.parse(path)
+    }
+
+    private fun uploadImageToFirebaseStorage(fileUri: Uri) {
+        val storageRef: StorageReference = FirebaseStorage.getInstance().reference
+        val imageName = UUID.randomUUID().toString()
+        val imageRef: StorageReference = storageRef.child("images/$imageName")
+
+        imageRef.putFile(fileUri)
+            .addOnSuccessListener { taskSnapshot ->
+                imageRef.downloadUrl.addOnSuccessListener { uri ->
+                    val imageUrl = uri.toString()
+                    saveDataToFirestore(imageUrl)
+                }
+            }
+            .addOnFailureListener { e ->
+                Toast.makeText(requireContext(), "Error al subir la imagen: ${e.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun saveDataToFirestore(imageUrl: String?) {
+        val menu = hashMapOf(
+            "imagen" to imageUrl,
+            "altura" to binding.userAltura.text.toString(),
+            "peso" to binding.userPeso.text.toString(),
+            "fechaCreacion" to FieldValue.serverTimestamp()
+        )
+
+        val db = FirebaseFirestore.getInstance()
+        db.collection("informacion")
+            .add(menu)
+            .addOnSuccessListener { documentReference ->
+                Toast.makeText(
+                    requireContext(),
+                    "Usuario guardado correctamente",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            .addOnFailureListener { e ->
+                Toast.makeText(
+                    requireContext(),
+                    "Error al guardar el usuario: ${e.message}",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+    }
+
+    private fun requestPermissions() {
+        val permissionsToRequest = mutableListOf<String>()
+
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            permissionsToRequest.add(Manifest.permission.CAMERA)
+        }
+
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            permissionsToRequest.add(Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
+
+        if (permissionsToRequest.isNotEmpty()) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                permissionsToRequest.toTypedArray(),
+                REQUEST_CODE_PERMISSIONS
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permiso concedido
+                Toast.makeText(requireContext(), "Permisos concedidos", Toast.LENGTH_SHORT).show()
+            } else {
+                // Permiso denegado
+                Toast.makeText(requireContext(), "Permisos denegados", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
