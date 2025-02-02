a5a5c1c7b86b48c13485f687cb13e0fb5a50e672
Miguel Angel <elguti17.mg@gmail.com>
2025-02-02 09:33:51 +0100

    EJERCICIO MULTIMEDIA GEOLOCALIZACION - VIDEOS FUNCIONAL

--- EjercicioMultimediaGeolocalizacion/app/src/main/java/com/example/ejerciciomultimediageolocalizacion/DetailActivity.kt
+++ EjercicioMultimediaGeolocalizacion/app/src/main/java/com/example/ejerciciomultimediageolocalizacion/DetailActivity.kt
@@ -1,8 +1,10 @@
 package com.example.ejerciciomultimediageolocalizacion
 
 import android.os.Bundle
+import android.widget.MediaController
 import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.ejerciciomultimediageolocalizacion.databinding.ActivityDetailBinding
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -12,24 +14,34 @@ import com.google.android.gms.maps.model.MarkerOptions
 
 class DetailActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var map: GoogleMap
+    private lateinit var binding: ActivityDetailBinding
     private lateinit var videoView: VideoView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val place = intent.getParcelableExtra<Place>("place")
 
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
-        videoView = findViewById(R.id.videoView)
-        videoView.setVideoPath(place?.videoUrl)
-        videoView.setOnPreparedListener { player ->
-            player.isLooping = true
-        }
-        videoView.start()
+        videoView = binding.videoView
+        initializePlayer(place?.videoUrl)
+    }
 
+    private fun initializePlayer(videoUrl: String?) {
+        if (videoUrl != null) {
+            videoView.setVideoPath(videoUrl)
+            val mediaController = MediaController(this)
+            videoView.setMediaController(mediaController)
+            mediaController.setAnchorView(videoView)
+            videoView.setOnPreparedListener { player ->
+                player.isLooping = true
+            }
+            videoView.start()
+        }
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -41,5 +53,3 @@ class DetailActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 }
-
-
--- EjercicioMultimediaGeolocalizacion/app/src/main/java/com/example/ejerciciomultimediageolocalizacion/MainActivity.kt
+++ EjercicioMultimediaGeolocalizacion/app/src/main/java/com/example/ejerciciomultimediageolocalizacion/MainActivity.kt
@@ -38,11 +38,11 @@ class MainActivity : AppCompatActivity() {
 
     private fun getPlaces(): List<Place> {
         return listOf(
-            Place("New York", R.drawable.new_york, LatLng(40.7128, -74.0060), "https://www.youtube.com/watch?v=h3fUgOKFMNU&t=11s"),
-            Place("Paris", R.drawable.paris, LatLng(48.8566, 2.3522), "https://www.youtube.com/watch?v=REDVbTQxMXo"),
-            Place("Tokyo", R.drawable.tokyo, LatLng(35.6895, 139.6917), "https://www.youtube.com/watch?v=6DQxRQb9dCE"),
-            Place("London", R.drawable.london, LatLng(51.5074, -0.1278), "https://www.youtube.com/watch?v=X8zLJlU_-60"),
-            Place("Sydney", R.drawable.sydney, LatLng(-33.8688, 151.2093), "https://www.youtube.com/watch?v=2QVwEWIKMI8")
+            Place("New York", R.drawable.new_york, LatLng(40.7128, -74.0060), "https://media.istockphoto.com/id/2120055196/es/v%C3%ADdeo/lapso-de-tiempo-de-la-multitud-de-turistas-que-caminan-y-cruzan-la-calle-en-times-square.mp4?s=mp4-640x640-is&k=20&c=JzpyvwzojlQE3BXcwehvEfD2f3ReNhfzxvHV9O8-tZ0="),
+            Place("Paris", R.drawable.paris, LatLng(48.8566, 2.3522), "https://media.istockphoto.com/id/2137222621/es/v%C3%ADdeo/eiffel-tower-in-paris.mp4?s=mp4-640x640-is&k=20&c=pNcfuTmjCesX6cZjYAbwNZLiqR50gGj6281TjUJNYls="),
+            Place("Tokyo", R.drawable.tokyo, LatLng(35.6895, 139.6917), "https://media.istockphoto.com/id/1735141079/es/v%C3%ADdeo/transporte-r%C3%A1pido-de-tren-shinkansen-de-alta-velocidad-en-tokio-por-la-noche-vista-del.mp4?s=mp4-640x640-is&k=20&c=xW1-fmjoTnSmQK5ddzXsRJJUraBFG1gRamPg58PnFmY="),
+            Place("London", R.drawable.london, LatLng(51.5074, -0.1278), "https://cdn.pixabay.com/video/2018/10/19/18792-296338745_large.mp4"),
+            Place("Sydney", R.drawable.sydney, LatLng(-33.8688, 151.2093), "https://www.shutterstock.com/shutterstock/videos/3601467551/preview/stock-footage-sydney-new-south-wales-australia-sydney-opera-house-one-of-the-landmarks-of.webm")
         )
     }
 }
